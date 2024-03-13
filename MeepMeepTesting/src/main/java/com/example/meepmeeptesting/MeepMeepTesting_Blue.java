f5d07f32e0d67cc0f6fc9c09a77e1454aff0f552
MustangRobotics20313 <grapevine.robotics@gmail.com>
2024-03-13 13:16:31 -0500

    meepmeep files for ctrl hub update

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting_Blue.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting_Blue.java
@@ -1,11 +1,12 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTesting_RF {
+public class MeepMeepTesting_Blue {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(800);
 
@@ -13,15 +14,10 @@ public class MeepMeepTesting_RF {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(-35, -50, (Math.toRadians(90))))
-                                .forward(24)
-                                .forward(-24)
-                                .turn(Math.toRadians(-90))
-                                .forward(80)
-                                .strafeLeft(15)
-                                .forward(10)
-                                .forward(-10)
-                                .strafeRight(15)
+                        drive.trajectorySequenceBuilder(new Pose2d(-34, 70, (Math.toRadians(-90))))
+                                .splineTo(new Vector2d(-55,30), Math.toRadians(-90))
+                                .forward(-3)
+                                .splineTo(new Vector2d(56,60), Math.toRadians(0))
                                 .build()
                 );
 
